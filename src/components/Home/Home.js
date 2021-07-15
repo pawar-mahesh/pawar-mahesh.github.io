662bf5c35c7498587cf9f61aa8c716d02586871c
pawar-mahesh <pawar.mahesh2345@gmail.com>
2021-07-15 14:35:53 +0530

    src: Home: Add Home Component
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "../Header/Header";
+import Greeting from "../Greeting/Greeting";
+import Skills from "../Skills/Skills";
+import Footer from "../Footer/Footer";
+import TopButton from "../TopButton/TopButton";
 
-const Home = () => {
-  return <div>This is home</div>;
+const Home = (props) => {
+  return (
+    <div>
+      <Header theme={props.theme} />
+      <Greeting theme={props.theme} />
+      <Skills theme={props.theme} />
+      <Footer theme={props.theme} />
+      <TopButton theme={props.theme} />
+    </div>
+  );
 };
 
 export default Home;
