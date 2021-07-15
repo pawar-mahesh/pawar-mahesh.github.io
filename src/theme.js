a8a7b671a62f667a849ff4194d6a6051bf088edd
pawar-mahesh <pawar.mahesh2345@gmail.com>
2021-07-15 19:19:18 +0530

    src: Add theme.js
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const blackTheme = {
+  body: "#E5E5E5",
+  text: "#14213d",
+  highlight: "#ffffff",
+  dark: "#000000",
+  secondaryText: "#5A6377",
+  imageHighlight: "#fca311",
+  compImgHighlight: "#E6E6E6",
+  jacketColor: "#8d99ae",
+  headerColor: "#fca31177",
+};
+
+export const chosenTheme = blackTheme;
