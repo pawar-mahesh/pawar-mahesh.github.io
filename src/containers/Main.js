d267eb018a9955a21e536fff07c05fea39df37c2
pawar-mahesh <pawar.mahesh2345@gmail.com>
2021-07-16 10:39:58 +0530

    src: Add splash component
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,15 +1,26 @@
 import React from "react";
-import Home from "../pages/home/HomeComponent";
 import { Route, Switch, HashRouter } from "react-router-dom";
+import Home from "../pages/home/HomeComponent";
+import Splash from "../pages/splash/Splash";
 
-const Main = () => {
+const Main = (props) => {
+  const theme = props.theme;
   return (
     <div>
       <HashRouter basename="/">
         <Switch>
           <Route
+            path="/"
+            exact
+            render={(props) => <Splash {...props} theme={theme} />}
+          />
+          <Route
+            path="/splash"
+            render={(props) => <Splash {...props} theme={theme} />}
+          />
+          <Route
             path="/home"
-            render={(props) => <Home {...props} theme={props.theme} />}
+            render={(props) => <Home {...props} theme={theme} />}
           />
         </Switch>
       </HashRouter>
