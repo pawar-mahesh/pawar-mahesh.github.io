b71490e4b3dca33457243457be8bd8f328274d7e
pawar-mahesh <pawar.mahesh2345@gmail.com>
2021-07-17 13:43:47 +0530

    src: home: Render All for home screen

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -4,6 +4,7 @@ import Greeting from "../../containers/greeting/Greeting";
 import Skills from "../../containers/skills/Skills";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
+import HomeComponentRenderAll from "./HomeComponentRenderAll";
 
 class Home extends Component {
   render() {
@@ -12,6 +13,7 @@ class Home extends Component {
         <Header theme={this.props.theme} />
         <Greeting theme={this.props.theme} />
         <Skills theme={this.props.theme} />
+        <HomeComponentRenderAll theme={this.props.theme} />
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
       </div>
---
+++ src/pages/home/HomeComponentRenderAll.js
@@ -0,0 +1,206 @@
+import React, { Component } from "react";
+import Educations from "../../containers/education/Educations";
+import Certifications from "../../containers/certifications/Certifications";
+import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
+import EducationImg from "../education/EducationImg";
+import { competitiveSites } from "../../portfolio";
+import "./../education/EducationComponent.css";
+import { Fade } from "react-reveal";
+import ExperienceAccordion from "../../containers/experienceAccordion/ExperienceAccordion.js";
+import "./../experience/Experience.css";
+import { experience } from "../../portfolio.js";
+import ExperienceImg from "../experience/ExperienceImg";
+import Button from "../../components/button/Button";
+import { projectsHeader } from "../../portfolio.js";
+import "../projects/Projects.css";
+import ProjectsImg from "../projects/ProjectsImg";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import AddressImg from "../contact/AddressImg";
+import "../contact/ContactComponent.css";
+import { greeting, contactPageData } from "../../portfolio.js";
+import "./HomeComponentRenderAll.css";
+
+const ContactData = contactPageData.contactSection;
+const addressSection = contactPageData.addressSection;
+const phoneSection = contactPageData.phoneSection;
+
+class HomeComponentRenderAll extends Component {
+  render() {
+    const theme = this.props.theme;
+    return (
+      <div className="education-main">
+        {/* Education */}
+        <div className="margin-up-space"></div>
+        <div className="basic-education">
+          <Fade bottom duration={2000} distance="40px">
+            <div className="heading-div">
+              <div className="heading-img-div">
+                <EducationImg theme={theme} />
+              </div>
+              <div className="heading-text-div">
+                <h1 className="heading-text" style={{ color: theme.text }}>
+                  Education
+                </h1>
+                <h3 className="heading-sub-text" style={{ color: theme.text }}>
+                  Basic Qualification and Certifcations
+                </h3>
+                <CompetitiveSites logos={competitiveSites.competitiveSites} />
+              </div>
+            </div>
+          </Fade>
+          <Educations theme={this.props.theme} />
+          <Certifications theme={this.props.theme} />
+        </div>
+
+        {/* Experience */}
+        <div className="margin-up-space"></div>
+        <div className="basic-experience">
+          <Fade bottom duration={2000} distance="40px">
+            <div className="experience-heading-div">
+              <div className="experience-heading-img-div">
+                <ExperienceImg theme={theme} />
+              </div>
+              <div className="experience-heading-text-div">
+                <h1
+                  className="experience-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {experience.title}
+                </h1>
+                <h3
+                  className="experience-heading-sub-text"
+                  style={{ color: theme.text }}
+                >
+                  {experience["subtitle"]}
+                </h3>
+                <p
+                  className="experience-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {experience["description"]}
+                </p>
+              </div>
+            </div>
+          </Fade>
+        </div>
+        <ExperienceAccordion sections={experience["sections"]} theme={theme} />
+
+        {/* Projects */}
+        <div className="margin-up-space"></div>
+        <div className="basic-projects">
+          <Fade bottom duration={2000} distance="40px">
+            <div className="projects-heading-div">
+              <div className="projects-heading-img-div">
+                <ProjectsImg theme={theme} />
+              </div>
+              <div className="projects-heading-text-div">
+                <h1
+                  className="projects-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {projectsHeader.title}
+                </h1>
+                <p
+                  className="projects-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {projectsHeader["description"]}
+                </p>
+              </div>
+            </div>
+          </Fade>
+        </div>
+
+        <Button
+          text={"Check Projects On Github"}
+          className="project-button"
+          href="https://github.com/pawar-mahesh?tab=repositories"
+          newTab={true}
+          theme={theme}
+        />
+
+        {/* Contact Me */}
+        <div className="margin-up-space"></div>
+        <div className="basic-contact">
+          <Fade bottom duration={1000} distance="40px">
+            <div className="contact-heading-div">
+              <div className="contact-heading-img-div">
+                <img
+                  src={require(`../../assests/images/${ContactData["profile_image_path"]}`)}
+                  alt=""
+                />
+              </div>
+              <div className="contact-heading-text-div">
+                <h1
+                  className="contact-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {ContactData["title"]}
+                </h1>
+                <p
+                  className="contact-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {ContactData["description"]}
+                </p>
+                <SocialMedia theme={theme} />
+                <div className="resume-btn-div">
+                  <Button
+                    text="See My Resume"
+                    newTab={true}
+                    href={greeting.resumeLink}
+                    theme={theme}
+                  />
+                </div>
+              </div>
+            </div>
+          </Fade>
+
+          <Fade bottom duration={1000} distance="40px">
+            <div className="address-heading-div">
+              <div className="contact-heading-img-div">
+                <AddressImg theme={theme} />
+              </div>
+              <div className="address-heading-text-div">
+                <h1
+                  className="address-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {addressSection["title"]}
+                </h1>
+                <p
+                  className="contact-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {addressSection["subtitle"]}
+                </p>
+                <h1
+                  className="address-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {phoneSection["title"]}
+                </h1>
+                <p
+                  className="contact-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {phoneSection["subtitle"]}
+                </p>
+                {/* <div className="address-btn-div">
+                  <Button
+                    text="Visit on Google Maps"
+                    newTab={true}
+                    href={addressSection.location_map_link}
+                    theme={theme}
+                  />
+                </div> */}
+              </div>
+            </div>
+          </Fade>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomeComponentRenderAll;
