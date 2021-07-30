823b6d61c4376cbf3d8995e0c1d20d7252134364
pawar-mahesh <pawar.mahesh2345@gmail.com>
2021-07-30 12:41:08 +0530

    src: Make LoaderLogo & Splash static
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- src/pages/splash/Splash.js
+++ src/pages/splash/Splash.js
@@ -6,7 +6,7 @@ import LoaderLogo from "../../components/Loader/LoaderLogo.js";
 function AnimatedSplash(props) {
   return (
     <div className="logo_wrapper">
-      <div className="screen" style={{ backgroundColor: "#99401F" }}>
+      <div className="screen" style={{ backgroundColor: "	#18191A" }}>
         <LoaderLogo id="logo" theme={props.theme} />
       </div>
     </div>
