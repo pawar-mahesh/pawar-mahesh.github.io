9fa226d900dfebef3d2ea5695ca42fe19b885d56
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-10-28 20:21:03 +0530

    Implement router using react-router-dom
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import ContactMe from "./components/pages/ContactMe";
+import Education from "./components/pages/Education";
+import Experience from "./components/pages/Experience";
+import Projects from "./components/pages/Projects";
+
 const App = () => {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact-me" element={<ContactMe />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
